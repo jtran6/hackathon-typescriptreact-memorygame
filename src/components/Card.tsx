5750d9c5d3832bb00903dbd8bd4e05af61eda67f
Josh Tran <Josh.Tran@procurementpartners.com>
2024-08-13 19:02:27 -0400

    somewhat working version

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,28 +1,32 @@
 import React, { useState } from 'react';
 import '../styles/Card.css';
 
-interface CardProps {
-  /*
-    This is the Card component that represents a single card in the game. We need to
-    track the state of the card to determine if it has been flipped or matched. We also
-    need to track the type of card to check if two cards are a match. If the cards are a match,
-    we can update the state of the cards to indicate that they are matched. If they are not a match,
-    we can flip the cards back over.
-  */
-  cardNumber: number;
+/*
+  This is the Card component that represents a single card in the game. We need to
+  track the state of the card to determine if it has been flipped or matched. We also
+  need to track the type of card to check if two cards are a match. If the cards are a match,
+  we can update the state of the cards to indicate that they are matched. If they are not a match,
+  we can flip the cards back over.
+*/
 
+interface CardProps {
+  cardNumber: number;
+  image: string;
+  isFlipped: boolean;
+  //isMatched: boolean;
 }
 
-const Card: React.FC<CardProps> = ({ cardNumber }) => {
+const Card: React.FC<CardProps> = ({ cardNumber, image }) => {
   const [isClicked, setIsClicked] = useState(false); // state to manage if card has been clicked
 
   const handleClick = () => {
-    setIsClicked(true); // set isClicked to true when card is clicked
+    console.log(`Card ${cardNumber} clicked`);
+    setIsClicked(!isClicked);
   };
 
   return (
     <div className="card" onClick={handleClick}>
-      Card {cardNumber} {isClicked && 'Clicked'}
+      {isClicked ? <img src={image} alt={`Card ${cardNumber}`} style={{ width: '100%', height: '100%' }} /> : `Card ${cardNumber}`}
     </div>
   );
 };
--- src/components/GameScreen.tsx
+++ src/components/GameScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { } from 'react';
 import '../styles/GameScreen.css';
 import Card from './Card'; // import the Card component
 
@@ -20,12 +20,50 @@ interface Props {
 }
 
 const GameScreen: React.FC<Props> = ({ x, y, onEndGame }) => {
+  //const [selectedCards, setSelectedCards] = useState<number[]>([]);
+  //const [matchedCards, setMatchedCards] = useState<number[]>([]);
+  //onst [flippedCards, setFlippedCards] = useState<number[]>([]);
+  //const cardRefs = useRef<(HTMLDivElement | null)[]>([]);
   const cards = Array.from({ length: x * y }, (_, i) => i + 1);
+  const birdImages = [
+    "https://cdn.pixabay.com/photo/2024/05/24/19/06/bird-8785666_1280.jpg",
+    "https://cdn.pixabay.com/photo/2019/03/28/18/13/bird-4087736_1280.jpg",
+    "https://cdn.pixabay.com/photo/2019/03/12/18/00/bird-4051412_1280.jpg",
+    "https://cdn.pixabay.com/photo/2024/01/15/14/59/bird-8510323_1280.jpg",
+    "https://cdn.pixabay.com/photo/2015/06/21/20/12/peafowl-816981_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2018/10/26/17/28/yellow-billed-hornbill-3774974_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2023/05/28/09/37/pelican-8023249_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2024/03/31/06/16/bird-8666099_1280.jpg"
+  ];
+
+  // const handleCardClick = (card: number) => {
+  //   console.log("Card clicked:", card);
+  // };
+
+  // useEffect(() => {
+  //   console.log("Selected cards:", selectedCards);
+  //   if (selectedCards.length === 2) {
+  //     const [firstCard, secondCard] = selectedCards;
+  //     if (firstCard !== secondCard) {
+  //       setTimeout(() => {
+  //         setFlippedCards(flippedCards.filter(card => card !== firstCard && card !== secondCard));
+  //         setSelectedCards([]);
+  //       }, 1000); // Adjust the delay as needed
+  //     } else {
+  //       setSelectedCards([]);
+  //     }
+  //   }
+  // }, [selectedCards, flippedCards]);
 
   return (
     <div className="gamescreen" style={{ gridTemplateColumns: `repeat(${x}, 1fr)` }}>
-      {cards.map((card) => (
-        <Card key={card} cardNumber={card} />
+      {cards.map((card, index) => (
+       <Card 
+       key={card}
+       cardNumber={card}
+       image={birdImages[index % birdImages.length]} // Use different images
+       isFlipped={false}
+     />
       ))}
       <button onClick={onEndGame}>End Game</button>
     </div>
